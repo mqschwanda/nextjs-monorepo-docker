877d9f209f8db21dcabf93e41dec46231a7a4dbd
mqschwanda <mqschwanda@gmail.com>
2023-05-11 18:10:07 -0400

    loadSchema glob

--- packages/graphql-schema/src/loadSchema.ts
+++ packages/graphql-schema/src/loadSchema.ts
@@ -1,12 +1,19 @@
 import { readFileSync } from 'fs';
+import { globSync } from 'glob';
 import { buildSchema } from 'graphql';
+import path from 'path';
+import { schemaGlob } from './constants';
 
-const schema = readFileSync(
-  require.resolve('./schema/schema.graphql'),
-  {
-    encoding: 'utf-8',
-  },
-);
+const filePaths = globSync(path.join(__dirname, schemaGlob));
+
+const schema = filePaths.map(
+  (filePath) => readFileSync(
+    filePath,
+    {
+      encoding: 'utf-8',
+    },
+  ),
+).join(' ');
 
 export default function loadSchema() {
   return buildSchema(schema);
