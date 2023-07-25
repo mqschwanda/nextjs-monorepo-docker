bd763a1d0a1e67adf7119668d7df5b95c1588e58
mqschwanda <mqschwanda@gmail.com>
2023-07-25 17:46:05 -0400

    setup user and roles

--- packages/prisma/src/client.ts
+++ packages/prisma/src/client.ts
@@ -1,5 +1,50 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, RoleKey, Prisma } from '@prisma/client';
 
-const prisma = new PrismaClient();
-// use `prisma` in your application to read and write data in your DB
+export * from '@prisma/client';
+
+const prisma = new PrismaClient()
+  .$extends({
+    model: {
+      user: {
+        async addUserRoleForUniqueUser({
+          roleKey,
+          where,
+        }: {
+          where: Prisma.Exact<Prisma.UserWhereUniqueInput, Prisma.UserWhereUniqueInput>,
+          roleKey: RoleKey
+        }) {
+          const [
+            adminUser,
+            adminRole,
+          ] = await Promise.all([
+            prisma.user.findUniqueOrThrow({
+              where,
+            }),
+            prisma.role.findUniqueOrThrow({
+              where: {
+                key: roleKey,
+              },
+            }),
+          ]);
+
+          await prisma.userRole.upsert({
+            create: {
+              roleId: adminRole.id,
+              userId: adminUser.id,
+            },
+            update: {},
+            where: {
+              userId_roleId: {
+                roleId: adminRole.id,
+                userId: adminUser.id,
+              },
+            },
+          });
+        },
+      },
+    },
+    name: '@mqs/prisma',
+  });
+
+export { prisma };
 export default prisma;
---
+++ packages/prisma/src/seed/index.ts
@@ -0,0 +1,65 @@
+import {
+  User,
+  Role,
+  RoleKey,
+  prisma,
+} from '../client';
+
+async function seed() {
+  const roles: Array<Omit<Role, 'id'>> = [
+    {
+      key: RoleKey.Admin,
+      name: 'admin',
+    },
+  ];
+
+  await Promise.all(
+    roles.map((role) => prisma.role.upsert({
+      create: role,
+      update: {},
+      where: {
+        key: role.key,
+      },
+    })),
+  );
+
+  const users: Array<Omit<User, 'id' | 'createdAt'>> = [
+    {
+      email: 'admin@email.com',
+      nameFirst: 'Admin',
+      nameLast: 'Smith',
+    },
+    {
+      email: 'user@email.com',
+      nameFirst: 'John',
+      nameLast: 'Smith',
+    },
+  ];
+
+  await Promise.all(
+    users.map((user) => prisma.user.upsert({
+      create: user,
+      update: {},
+      where: {
+        email: user.email,
+      },
+    })),
+  );
+
+  await prisma.user.addUserRoleForUniqueUser({
+    roleKey: RoleKey.Admin,
+    where: {
+      email: 'admin@email.com',
+    },
+  });
+}
+
+seed()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (error) => {
+    console.error(error); // eslint-disable-line no-console
+    await prisma.$disconnect();
+    process.exit(1);
+  });
