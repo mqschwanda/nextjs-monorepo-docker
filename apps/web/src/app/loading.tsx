c3ee486bf69d221933c8045abb8e104e02114469
mqschwanda <mqschwanda@gmail.com>
2023-08-01 02:24:19 -0400

    add loading

--- apps/web/src/app/loading.tsx
+++ apps/web/src/app/loading.tsx
@@ -1,7 +1,6 @@
 import { LoadingIndicator } from '@mqs/react-server-components';
 
 export default function Loading() {
-  // Or a custom loading skeleton component
   return (
     <div
       className='flex w-full h-full'
