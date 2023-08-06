a24105d4b2a7d2cfe9130831d6e0cbcdc02fb1d0
mqschwanda <mqschwanda@gmail.com>
2023-08-06 02:14:20 -0400

    refactor middleware options pattern

--- packages/graphql-server/src/resolvers/middleware/authenticate/index.ts
+++ packages/graphql-server/src/resolvers/middleware/authenticate/index.ts
@@ -10,50 +10,51 @@ type AuthenticateOptions = {
   throwErrors: boolean
 };
 
-async function authenticateResolver<
+const authenticateResolver = <
   TParent extends {},
   TContext extends ContextType,
   TArgs extends {},
   >(
-  parent: TParent,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo,
-  options: AuthenticateOptions = {
-    throwErrors: true,
-  },
-) {
-  const cookies = context.request.headers.get('cookie');
+    options: AuthenticateOptions = {
+      throwErrors: true,
+    },
+  ) => async (
+    parent: TParent,
+    args: TArgs,
+    context: TContext,
+    info: GraphQLResolveInfo,
+  ) => {
+    const cookies = context.request.headers.get('cookie');
 
-  if (cookies) {
-    const { access } = cookie.parse(cookies);
+    if (cookies) {
+      const { access } = cookie.parse(cookies);
 
-    if (access) {
-      try {
-        const token = await Tokens.verifyAccessToken(access);
+      if (access) {
+        try {
+          const token = await Tokens.verifyAccessToken(access);
 
-        context.user = token.user;
-      } catch (error) {
-        if (options.throwErrors) {
-          throw error;
+          context.user = token.user;
+        } catch (error) {
+          if (options.throwErrors) {
+            throw error;
+          }
         }
       }
     }
-  }
 
-  if (context.user || !options.throwErrors) {
-    return [parent, args, context, info] as const;
-  }
+    if (context.user || !options.throwErrors) {
+      return [parent, args, context, info] as [TParent, TArgs, TContext, GraphQLResolveInfo];
+    }
 
-  throw new AuthenticationError();
-}
+    throw new AuthenticationError();
+  };
 
 export const authenticate = <
   TResult extends any,
   TParent extends {},
   TContext extends ContextType,
   TArgs extends {},
->(
+  >(
     resolver: Resolver<TResult, TParent, TContext, TArgs>,
     options?: AuthenticateOptions,
   ): Resolver<TResult, TParent, TContext, TArgs> => async (
@@ -63,7 +64,7 @@ export const authenticate = <
     info: GraphQLResolveInfo,
   ) => resolveResolver(
     resolver,
-    ...await authenticateResolver(parent, args, context, info, options),
+    ...await authenticateResolver<TParent, TContext, TArgs>(options)(parent, args, context, info),
   );
 
 export default authenticate;
